7d38f5f94ba518cee4973169f4d1ee3fbc2a86e5
Minhwan Lee <49065750+kas136@users.noreply.github.com>
2021-02-05 17:43:24 +0900

    Greedy

---
+++ Programmers/구명보트.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+
+        Arrays.sort(people);
+        int right = people.length - 1;
+        int left = 0;
+
+        while(right >= left) {
+            int sum = people[right--];
+            if(sum + people[left] <= limit)
+                left++;
+
+            answer++;
+        }
+
+        return answer;
+    }
+}
